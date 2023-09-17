e9c1f6a0d76ef84d98cb53218b1361c8fad88d9d
Zen <ztang230@gmail.com>
2023-09-16 21:14:46 -0400

    prevent overscroll

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className="h-full overscroll-y-none">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
