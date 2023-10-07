eb2279cf0eab883e524594391282fd1b2c3c29c1
Zen <ztang230@gmail.com>
2023-10-07 16:46:46 -0400

    re

--- app/routes/sandbox.playground.tsx
+++ app/routes/sandbox.playground.tsx
@@ -102,17 +102,74 @@ function ComboBox() {
   );
 }
 
+function MenuBar() {
+  return (
+    <div className="flex p-4 items-center gap-2">
+      <div className="text-lg font-medium mr-auto">Playground</div>
+      <ComboBox />
+      <Button variant="secondary">Save</Button>
+      <Button variant="secondary">View code</Button>
+      <Button variant="secondary">Share</Button>
+      <Button variant="secondary">...</Button>
+    </div>
+  );
+}
 export default function Page() {
+  const sidebar = (
+    <div className="grid gap-6">
+      <div className="flex flex-col space-y-2">
+        <Label>Mode</Label>
+        <Tabs>
+          <TabsList>
+            <TabsTrigger value="account">Account</TabsTrigger>
+            <TabsTrigger value="password">Password</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <Label>Model</Label>
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="Select a model" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="003">textr-davinci-003</SelectItem>
+            <SelectItem value="002">text-ada-002</SelectItem>
+            <SelectItem value="004">text-gpt-004</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {/* TODO: break these out into components! that display the current value */}
+      <div className="flex flex-col space-y-4">
+        <div className="flex justify-between">
+          <Label>Temperature</Label>
+          <div className="text-sm opacity-50">200</div>
+        </div>
+        <Slider defaultValue={[33]} max={40} step={1} />
+      </div>
+
+      <div className="flex flex-col space-y-4">
+        <div className="flex justify-between">
+          <Label>Maximum Length</Label>
+          <div className="text-sm opacity-50">200</div>
+        </div>
+        <Slider defaultValue={[33]} max={100} step={1} />
+      </div>
+
+      <div className="flex flex-col space-y-4">
+        <div className="flex justify-between">
+          <Label>Top P</Label>
+          <div className="text-sm opacity-50">0.9</div>
+        </div>
+        <Slider defaultValue={[33]} max={100} step={1} />
+      </div>
+    </div>
+  );
+
   return (
     <div>
-      <div className="flex p-4 items-center gap-2">
-        <div className="text-lg font-medium mr-auto">Playground</div>
-        <ComboBox />
-        <Button variant="secondary">Save</Button>
-        <Button variant="secondary">View code</Button>
-        <Button variant="secondary">Share</Button>
-        <Button variant="secondary">...</Button>
-      </div>
+      <MenuBar />
       <Separator />
       <div className="border rounded-lg mx-auto max-w-screen-lg grid grid-cols-[1fr,200px] p-6 gap-6">
         <form className="flex flex-col gap-4">
@@ -125,55 +182,7 @@ export default function Page() {
             <Button variant="secondary">Cancel</Button>
           </div>
         </form>
-        <div className="grid gap-6">
-          <div className="flex flex-col space-y-2">
-            <Label>Mode</Label>
-            <Tabs>
-              <TabsList>
-                <TabsTrigger value="account">Account</TabsTrigger>
-                <TabsTrigger value="password">Password</TabsTrigger>
-              </TabsList>
-            </Tabs>
-          </div>
-          <div className="flex flex-col space-y-2">
-            <Label>Model</Label>
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select a model" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="003">textr-davinci-003</SelectItem>
-                <SelectItem value="002">text-ada-002</SelectItem>
-                <SelectItem value="004">text-gpt-004</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
-
-          {/* TODO: break these out into components! that display the current value */}
-          <div className="flex flex-col space-y-4">
-            <div className="flex justify-between">
-              <Label>Temperature</Label>
-              <div className="text-sm opacity-50">200</div>
-            </div>
-            <Slider defaultValue={[33]} max={40} step={1} />
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <div className="flex justify-between">
-              <Label>Maximum Length</Label>
-              <div className="text-sm opacity-50">200</div>
-            </div>
-            <Slider defaultValue={[33]} max={100} step={1} />
-          </div>
-
-          <div className="flex flex-col space-y-4">
-            <div className="flex justify-between">
-              <Label>Top P</Label>
-              <div className="text-sm opacity-50">0.9</div>
-            </div>
-            <Slider defaultValue={[33]} max={100} step={1} />
-          </div>
-        </div>
+        {sidebar}
       </div>
     </div>
   );
